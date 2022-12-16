b1528eabb61f11701e7759e3985b0e470a1c9e8e
Nemesis <lorenzo.balugani@gmail.com>
2022-12-16 18:44:50 +0100

    :sparkles: Add address input on landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,20 +1,48 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Style from "./Landing.module.css";
-import {Heading, Panel,} from "@steffo/bluelib-react";
+import {Button, Form, Heading, Panel,} from "@steffo/bluelib-react";
 import {useAppContext} from "../libs/Context";
 import {useNavigate} from "react-router-dom";
+import {schema} from "../env";
 
 export default function Landing() {
     const {address, setAddress} = useAppContext()
     const navigator = useNavigate()
+    const [addr, setAddr] = useState("");
 
     useEffect( () => {
         if (localStorage.getItem("address") && address == null) {
             let address = localStorage.getItem("address")
             setAddress(address)
-            navigator("/login")
+            navigator("/srv/login")
         }
     })
+
+    async function conn(){
+        try{
+            const response = await fetch(schema + addr + "/api/server/v1/planetarium", {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': process.env.DOMAIN
+                },
+            });
+            if (response.status === 200) {
+                let values = await response.json()
+                setAddress(addr)
+                localStorage.setItem("address", addr)
+                navigator("/srv/login")
+            }
+            else{
+                alert("No instance running at address.")
+            }
+        }
+        catch (e) {
+            alert("No instance running at address.")
+        }
+    }
 
     return(
         <div className={Style.Landing}>
@@ -26,7 +54,19 @@ export default function Landing() {
 
             </div>
             <Panel style={{minWidth: "unset"}}>
-                "A Balocco and some more."
+                To connect to an instance, write the backend address below and click on "Connect".
+                <Form>
+                    <Form.Row>
+                        <Form.Field onSimpleChange={e => setAddr(e)} value={addr} required={true}
+                                    placeholder={""} label={"Address"}>
+                        </Form.Field>
+                    </Form.Row>
+                    <Button bluelibClassNames={"color-lime"} onClick={() => {
+                        conn()
+                    }}>
+                        {"Connect"}
+                    </Button>
+                </Form>
             </Panel>
         </div>
     );
